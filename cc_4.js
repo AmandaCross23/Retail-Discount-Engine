ea9a30f5c5e2d961a1b7b91e90bd3fb6dbbc410a
Amanda Cross <amandacross@usf.edu>
2025-09-21 22:09:36 -0400

    Added more code to finish the assignment. Also, added notes to show the different areas of code and what they represent.

--- cc_4.js
+++ cc_4.js
@@ -1,3 +1,4 @@
+//This is the products that will be used during the code
 const products = [
     {name: "applewatch", category: "electronics", price: 209.99, inventory: 10},
     {name: "jackets", category: "apparel", price: 30.99, inventory: 30},
@@ -5,7 +6,7 @@ const products = [
     {name: "mop", category: "household", price: 25.99, inventory: 15},
     {name: "apples", category: "groceries", price: 6.99, inventory: 50}
 ];
-
+// This show the switch being used to give the discounts
 for(let product of products) {
     let discount = 0;
     switch (product.category) {
@@ -23,50 +24,49 @@ for(let product of products) {
             discount = 0;
             break;
     }
-    product.promoPrice = (product.price * (1 - discount).toFixed(2));
+    product.promoPrice = (product.price * (1 - discount));
 }
-
+// This is the 3 customers and there cart
 const customers = [
     {customer_ID: 1, customerType: "regular", cart:[{name: "applewatch", qty: 1}, {name: "jackets", qty: 3}] },
     {customer_ID: 2, customerType: "student", cart:[{name: "cookies", qty: 2}, {name: "mop", qty: 1}] },
     {customer_ID: 3, customerType: "senior",  cart:[{name: "apples", qty: 4}, {name: "applewatch", qty: 1}] }
 ]
+// This shows step 4 and 5 showing the customers purchases and final total
+for (let customer of customers) {
+    let extraDiscount = 0;
+    if (customer.customerType === "student") {extraDiscount = 0.05;}
+    else if (customer.customerType === "senior") {extraDiscount = 0.07;}
+    else {extraDiscount = 0;}
 
-for (let customer in customers ) {
-    let orderLevel_discount = 0;
-    if (customer.customerType == "student") {
-        orderLevel_discount = 0.05;  
-    }
-    else if (customer.customerType == "senior"){
-        orderLevel_discount = 0.07;
-    }
-    else {
-        orderLevel_discount = 0
-    }
+    let subtotal = 0;
 
-
-    for (let cartItem in customers.cart){
-        let total = 0;
-        let product = products.find(products => product.name === cartItem.name);
-        if(product && products.inventory >= cartItem.qty){
-            total += product.promoPrice * cartItem.qty;
-            products.inventory -= cartItem.qty;
-    }
+    for (let item of customer.cart) {
+        let product = products.find(product => product.name === item.name);
+        if (product && product.inventory >= item.qty) {
+            subtotal += product.promoPrice * item.qty;
+            product.inventory -= item.qty;
         }
+        else {
+            subtotal = 0;
+        }
+    }
+    let finalTotal = subtotal * (1 - extraDiscount);
 
-
-    final_total = total - (total * orderLevel_discount);
-
-    console.log (`Customer ${customers.customer_ID} total order comes out to $${final_total}`);
+    console.log(`Customer ${customer.customer_ID}: $${finalTotal.toFixed(2)}`);
 }
-
+// Shows the key/value pair for a single product after discounts are applied
+console.log(`\nSingle product after discounts are applied:`)
 let singleproducts = products[0];
 for (let key in singleproducts) {
     console.log(`${key}: ${singleproducts[key]}`);
 }
-
-for (let product in products){
+// This shows the updated inventory after the customers purchased the products
+console.log(`\nAll product inventory:`)
+for (let product of products){
+    console.log(`\nProduct:`);
     for(let [key, value] of Object.entries(product)){
-        console.log(`${key}: ${value}`);
+        console.log(` ${key}: ${value}`);
     }
 }
+
